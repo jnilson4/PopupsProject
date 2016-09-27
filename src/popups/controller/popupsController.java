b4e1a12ebf98b4a33f5b93bf955b4deda0d8550e
Jake Nilson <jnil0572@740s-pl-132.local>
2016-09-27 13:08:01 -0600

    Added classes and imported to run the view package.

--- src/popups/controller/popupsController.java
+++ src/popups/controller/popupsController.java
@@ -1,9 +1,18 @@
 package popups.controller;
 
+import popups.view.PopupViewer;
+
 public class PopupsController
 {
+	private PopupViewer display;
+	
+	public PopupsController()
+	{
+		display = new PopupViewer();
+	}
+	
 	public void start()
 	{
-		
+		display.displayMessage("Look words on the monitor!!!");
 	}
 }
