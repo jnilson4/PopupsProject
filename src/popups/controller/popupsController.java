e18d4cdc64bdbe935b5814681413378fcd5edf8a
Jake Nilson <jnil0572@740s-pl-132.local>
2016-09-29 12:23:05 -0600

    Added a input verification for double values.

--- src/popups/controller/popupsController.java
+++ src/popups/controller/popupsController.java
@@ -24,4 +24,26 @@ public class PopupsController
 		count++;
 		}
 	}
+	
+	/**
+	 * Checks a supplied String value to see if it can be converted to a Double.
+	 * @param potentialValue The supplied String.
+	 * @return Whether the conversion to a double is possible as a boolean value.
+	 */
+	private boolean isDouble(String potentialValue)
+	{
+		boolean validDouble = false;
+		
+		try 
+		{
+			double testValue = Double.parseDouble(potentialValue);
+			validDouble = true;
+		}
+		catch(NumberFormatException notDoubleError)
+		{
+			display.displayMessage("That was not a double =:<");
+		}
+		
+		return validDouble;
+	}
 }
